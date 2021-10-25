b45b940255833cae81290b76717c5c843abb7f24
Hugo Lima Romão <hugo8romao@gmail.com>
2021-10-24 21:37:58 -0400

    yarn add progressbar

---
+++ src/templates/Places/index.tsx
@@ -0,0 +1,59 @@
+import Image from 'next/image'
+
+import { CloseOutline } from '@styled-icons/evaicons-outline'
+import LinkWrapper from 'components/LinkWrapper'
+
+import * as S from './styles'
+import { useRouter } from 'next/dist/client/router'
+
+/* eslint-disable @next/next/no-img-element */
+type ImageProps = {
+  url: string
+  height: number
+  width: number
+}
+
+export type PlacesTemplateProps = {
+  place: {
+    slug: string
+    name: string
+    description?: {
+      html: string
+    }
+    gallery: ImageProps[]
+  }
+}
+
+export default function PlacesTemplate({ place }: PlacesTemplateProps) {
+  const routes = useRouter()
+
+  if (routes.isFallback) return null
+
+  return (
+    <>
+      <LinkWrapper href="/">
+        <CloseOutline size={32} />
+      </LinkWrapper>
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>{place.name}</S.Heading>
+          <S.Body
+            dangerouslySetInnerHTML={{ __html: place.description?.html || '' }}
+          />
+          <S.Gallery>
+            {place.gallery.map((image, index) => (
+              <Image
+                key={`photo-${index}`}
+                src={image.url}
+                alt={place.name}
+                width={1000}
+                height={600}
+                quality={75}
+              />
+            ))}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
+    </>
+  )
+}
